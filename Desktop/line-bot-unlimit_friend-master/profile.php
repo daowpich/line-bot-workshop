7b4e5886f0ab1836145df5ea790815a30539ea1b
daowpich <46663645+daowpich@users.noreply.github.com>
2019-01-14 11:27:41 +0700

    Update value

--- Desktop/line-bot-unlimit_friend-master/profile.php
+++ Desktop/line-bot-unlimit_friend-master/profile.php
@@ -1,9 +1,9 @@
 <?php
 
 
-$access_token = '*Channel_Access_Token*';
+$access_token = '0VffB2uHACCIqUBoNLC/K86so+1oGrRgSiQUddphXsSk7Jhd1GbyiXjftW6E8dKAxwlLwCZ5zd6H+IWEWn/KKoMdHseKHOACmGRjcbVKtP2Spm+hjac0K+77DVYGoEGrnuP/OfsDF2enw6dSHQYpCQdB04t89/1O/w1cDnyilFU=';
 
-$userId = '*User_ID*';
+$userId = 'Ufe1ad0e4d9727c5fcf6f4e2f82aaa4c7';
 
 $url = 'https://api.line.me/v2/bot/profile/'.$userId;
 
